864d9e0126d1a078dbbccc613ff86195e0900f12
pashamakhilkumarreddy <pashamakhilkumarreddy@gmail.com>
2021-03-25 13:48:16 +0530

    add netlify build script

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,4 @@
-const config = {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   STRIPE_KEY: process.env.REACT_APP_STRIPE_PUBLIC_KEY,
   s3: {
     REGION: process.env.REACT_APP_REGION,
@@ -16,5 +15,28 @@ const config = {
     IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID,
   },
 }
+
+const prod = {
+  STRIPE_KEY: process.env.REACT_APP_PROD_STRIPE_PUBLIC_KEY,
+  s3: {
+    REGION: process.env.REACT_APP_PROD_REGION,
+    BUCKET: process.env.REACT_APP_PROD_BUCKET,
+  },
+  apiGateway: {
+    REGION: process.env.REACT_APP_PROD_REGION,
+    URL: process.env.REACT_APP_PROD_URL,
+  },
+  cognito: {
+    REGION: process.env.REACT_APP_PROD_REGION,
+    USER_POOL_ID: process.env.REACT_APP_PROD_USER_POOL_ID,
+    APP_CLIENT_ID: process.env.REACT_APP_PROD_APP_CLIENT_ID,
+    IDENTITY_POOL_ID: process.env.REACT_APP_PROD_IDENTITY_POOL_ID,
+  },
+}
+
+const config = {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...(process.env.REACT_APP_STAGE === 'prod' ? prod : dev),
+};
 
 export default config;
